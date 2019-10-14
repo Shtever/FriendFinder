95a74ad6965493c90105054a9ea6b1e05b36ab39
Stephen McMillen <Shtevetm@gmail.com>
2019-10-13 21:36:15 -0500

    matching is functional. Output to console.log. This code is suuuuuuper jenky

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,6 +6,21 @@ var users = [
         photo: "https://image-cdn.neatoshop.com/styleimg/87538/none/red/default/418831-20;1558720085t.jpg",
         scores: [1,1,1,1,1,1,1,1,1,1]
     },
+    {
+        name: "TWOS",
+        photo: "https://image-cdn.neatoshop.com/styleimg/87538/none/red/default/418831-20;1558720085t.jpg",
+        scores: [2,2,2,2,2,2,2,2,2,2]
+    },
+    {
+        name: "THREES",
+        photo: "https://image-cdn.neatoshop.com/styleimg/87538/none/red/default/418831-20;1558720085t.jpg",
+        scores: [3,3,3,3,3,3,3,3,3,3]
+    },
+    {
+        name: "FOURS",
+        photo: "https://image-cdn.neatoshop.com/styleimg/87538/none/red/default/418831-20;1558720085t.jpg",
+        scores: [4,4,4,4,4,4,4,4,4,4]
+    },
 
     {
         name: "A Quokka",
