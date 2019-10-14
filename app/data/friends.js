8d623bd558d3fc5c9ec53b76cd43acde4b4f862e
Stephen McMillen <Shtevetm@gmail.com>
2019-10-13 20:27:52 -0500

    for loops functoning.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,19 @@
 
 
-var users = []
+var users = [
+    {
+        name: "An Antisocial Robot",
+        photo: "https://image-cdn.neatoshop.com/styleimg/87538/none/red/default/418831-20;1558720085t.jpg",
+        scores: [1,1,1,1,1,1,1,1,1,1]
+    },
+
+    {
+        name: "A Quokka",
+        photo: "https://i.imgur.com/ax6lkYp.jpg",
+        scores: [5,5,5,5,5,5,5,5,5,5]
+    },
+
+]
 
 module.exports = users;
+
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,18 @@
 var userData = require("../data/friends");
 
+var express = require("express");
+var app = express();
+app.use(express.static("public"));
+
 module.exports = function (app) {
 
 app.get("/api/users", function (req, res) {
     res.json(userData);
 });
-}
+
+
+app.post("/api/users", function(req, res) {
+      userData.push(req.body);
+      res.json(true);
+    })
+};
